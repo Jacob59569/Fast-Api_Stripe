228d52c35e3cc3a5cb707c32bae5d6746e593d8f
Jacob <110312077+Jacob59569@users.noreply.github.com>
2025-06-08 20:32:37 +0300

    Add рекваеры

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 import stripe
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()  # Загружаем переменные из .env
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "Stripe + FastAPI = ❤️"}
-
 
 @app.post("/create-checkout-session")
 async def create_checkout_session():
@@ -38,3 +36,41 @@ async def create_checkout_session():
         return {"checkout_url": session.url}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/webhook")
+async def stripe_webhook(request: Request):
+    payload = await request.body()
+    sig_header = request.headers.get('stripe-signature')
+    webhook_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, webhook_secret
+        )
+    except stripe.error.SignatureVerificationError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        print("💰 Payment successful!")
+        print("Customer email:", session["customer_details"]["email"])
+        print("ID сессии:", session["id"])
+
+    return {"status": "success"}
+
+templates = Jinja2Templates(directory="templates")
+
+# Отдаём главную страницу
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Страница успеха
+@app.get("/success", response_class=HTMLResponse)
+async def success(request: Request):
+    return templates.TemplateResponse("success.html", {"request": request})
+
+# Страница отмены
+@app.get("/cancel", response_class=HTMLResponse)
+async def cancel(request: Request):
+    return templates.TemplateResponse("cancel.html", {"request": request})
